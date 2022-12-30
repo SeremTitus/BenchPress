01ec99d659fe5de286b5bf9060906e9f92c916e1
Serem Titus(serem_empire) <seremempire@gmail.com>
2022-12-31 02:42:54 +0300

    add: topbar reproduceable signals out topbar items

--- scenes/main/topbar.gd
+++ scenes/main/topbar.gd
@@ -1,69 +1,185 @@
 extends Control
 
-
-onready var file = $"%file"
-onready var edit = $"%edit"
-onready var debug = $"%debug"
-onready var editor = $"%editor"
-onready var help = $"%help"
-onready var device = $"%device"
-
-
+var pressed_signal_redirect={}#{itemName:itemIndex{callinstruction}}
 func _ready():
-	file_item()
-	edit_item()
-	debug_item()
-	editor_item()
-	help_item()
-	device_item()
-	
-func file_item():
-	file.get_popup().add_item("New")
-	file.get_popup().add_item("Open")
-	file.get_popup().add_item("Save")
-	file.get_popup().add_item("Append")
-	file.get_popup().add_item("Export")
-	file.get_popup().add_item("Quit")
-	
-func edit_item():
-	edit.get_popup().add_item("Undo")
-	edit.get_popup().add_item("Redo")
-	edit.get_popup().add_item("Fild")
-	edit.get_popup().add_item("Rename")
-	edit.get_popup().add_item("Cut")
-	edit.get_popup().add_item("Copy")
-	edit.get_popup().add_item("Paste")
-	edit.get_popup().add_item("Select All")
-	edit.get_popup().add_item("Inverse Selection")
-	edit.get_popup().add_item("Clear Selection")
-	
-	
-func debug_item():
-		debug.get_popup().add_item("Select Device")
-		debug.get_popup().add_item("Run")
-		debug.get_popup().add_item("Run by Step")
-		debug.get_popup().add_item("Pause")
-		debug.get_popup().add_item("Continue")
-		debug.get_popup().add_item("Stop")
-		debug.get_popup().add_item("Rerun")
-		debug.get_popup().add_item("Record")
-		debug.get_popup().add_item("Skip Breakpoints")
-		debug.get_popup().add_item("Skip Into")
-		
-func editor_item():
-	editor.get_popup().add_item("Editor Settings")
-	editor.get_popup().add_item("Editor Layout")
-	editor.get_popup().add_item("Open Editor Data")
-	editor.get_popup().add_item("Manage Editor Feature")
-	editor.get_popup().add_item("Manage Editor Extensions")
+	var topbar ={
+		$"%file":{
+			'New':{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Open":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Save":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Append":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Export":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Quit":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			},
+		$"%edit":{
+			'Undo':{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Redo":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Find":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Cut":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Copy":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Paste":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Select All":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Inverse Selection":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Clear Selection":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			},
+		$"%debug":{
+			'Select Device':{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Run":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Run by Step":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Pause":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Continue":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Stop":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Rerun":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Record":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Skip Breakpoints":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Skip Into":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			},
+		$"%editor":{
+			'Editor Settings':{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Editor Layout":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Open Editor Data":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Manage Editor Feature":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Manage Editor Extensions":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			},
+		$"%help":{
+			"Documentation":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"Report a Bug":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			"About BenchPress":{
+				'add_item':{},
+				'set_item_disabled':false,
+				'connect':{'target':$"%About",
+							'method': 'popup',
+							},
+				},
+			"Support Development":{
+				'add_item':{},
+				'set_item_disabled':true,
+				},
+			},
+		$"%device":{
+			'Local':{
+				'add_radio_check_item':{},
+				'set_item_disabled':true,
+				},
+			},
+	}
+	add_item(topbar)
 
-func help_item():
-	help.get_popup().add_item("Documentation")
-	help.get_popup().add_item("Report a Bug")
-	help.get_popup().add_item("About BenchPress")
-	help.get_popup().add_item("Support Development")
-	
-func device_item():
-	device.get_popup().add_radio_check_item("Local")
-	device.get_popup().set_item_disabled(0,true)
-	
+func add_item(topbar:Dictionary):
+	for menubutton in topbar:
+		var itemIndex = 0
+		for itemName in topbar[menubutton]:
+			for instruction in topbar[menubutton][itemName]:
+				var instructionValue = topbar[menubutton][itemName][instruction]
+				match instruction:
+					'add_item':
+						menubutton.get_popup().add_item(itemName,itemIndex)
+					'set_item_disabled':
+						menubutton.get_popup().set_item_disabled(itemIndex,bool(instructionValue))
+					'add_radio_check_item':
+						menubutton.get_popup().add_radio_check_item(itemName,itemIndex)
+					'connect':
+						pressed_signal_redirect[itemName]={itemIndex:instructionValue}
+						menubutton.get_popup().connect('index_pressed',self,'pressed',[itemName])
+			itemIndex += 1
+
+func pressed(itemIndex,itemName):
+	for ind in pressed_signal_redirect[itemName]:
+		if ind == itemIndex:
+			pressed_signal_redirect[itemName][itemIndex]['target'].call(pressed_signal_redirect[itemName][itemIndex]['method'])
