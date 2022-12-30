a652ad948cbe37fdeec26dddba2680df9812a46c
Serem Titus(serem_empire) <seremempire@gmail.com>
2022-12-31 02:47:56 +0300

    add: hide and unhide properties panel

--- scenes/main/Editor.gd
+++ scenes/main/Editor.gd
@@ -1,1 +1,11 @@
 extends Control
+
+onready var Properties = $"%Properties"
+
+func _ready():
+	# warning-ignore:return_value_discarded
+	ProjectSettings.connect("project_settings_changed",self,"update_from_settings")
+
+func update_from_settings():
+	$"%Dock".set_control_hidden($"%Properties",not ProjectSettings.get_setting('benchpress/properties/dock_visible'))
+	
