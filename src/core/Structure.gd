66d6c65026789d94679f9e268c60e4244bfc14b7
Serem Titus(SeremTitus) <seremtitusdev@gmail.com>
2023-12-04 03:37:17 +0300

    rename files

--- src/core/Structure.gd
+++ src/core/Structure.gd
@@ -5,10 +5,10 @@ var library_name:StringName #eg Built-in
 var description: String
 var icon: Texture2D
 var group_path: String
-var py_include:PackedStringArray
-var structure_include:Array[ElementStructure]
-var holds_child_element: bool = false
-var code: String # complies to a function
+var py_include:PackedStringArray# complies to importes
 var file_include:Array[PyScript]# complies to a create a file that is imported
+var structure_include:Array[ElementStructure] # also compiled
+var holds_child_element: bool = false # called in a if statement return true or fasle 
+var code: String # complies to a function
 var properties:Array[Attribute]# complies to a function args
 var output_properties:Array[Attribute]#Always Attribute.type = types.Text
