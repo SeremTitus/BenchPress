244c2a650a0066d11083df716e2df2b229f69c6d
Serem Titus(SeremTitus) <seremtitusdev@gmail.com>
2023-12-09 22:27:51 +0300

    ElementTree search

--- src/UI/elements_tree/ElementTree.gd
+++ src/UI/elements_tree/ElementTree.gd
@@ -1,14 +1,15 @@
 extends PanelContainer
 
+@export var sort_type:GlobalLibrary.SortMode = GlobalLibrary.SortMode.None
+
 @onready var tree:TrueTree = %TrueTree
 
 func _ready():
 	GlobalLibrary.library_updated.connect(populate_tree)
 	populate_tree()
 	
-func populate_tree() -> void:
+func populate_tree(library:Array[Structure] = GlobalLibrary.get_sorted(sort_type)) -> void:
 	tree.remove_all_clildren()
-	var library = GlobalLibrary.get_sorted(GlobalLibrary.SortMode.None)
 	for structure:Structure in library:
 		var item:CustomStructureItem = CustomStructureItem.create(
 			structure.title,
@@ -17,3 +18,9 @@ func populate_tree() -> void:
 			structure.icon
 			)
 		tree.add_item(item,structure.group_path)
+
+func search_tree(search:String) -> void:
+	if search.is_empty():
+		populate_tree()
+	else:
+		populate_tree(GlobalLibrary.get_search(search,sort_type))
